3122e2c2347f6ef24113d76e7e2f92b4c95dd436
Chanly Ly <lychanly4@gmail.com>
2021-02-11 17:35:17 -0800

    worked on custom element

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-  fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => localStorage.setItem('ShopItems',data));
+  if (localStorage.getItem('ShopItems') != null) {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('ShopItems', JSON.stringify(data)));
+  }
+  
+  for (item in JSON.parse(localStorage.getItem('ShopItems'))) {
+    let product = document.createElement('product-item');
+    product.setAttribute('src', item['image']);
+    product.setAttribute('alt', item['title']);
+    product.setAttribute('title', item['title']);
+    product.setAttribute('price', item['price']);
+    productList = document.getElementById('product-list');
+    productList.appendChild(product);
+  }
 });
